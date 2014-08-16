9547917d178604c32d6a25d8dbe6589bef972198
Stephen Zhou <zhoufwind@hotmail.com>
2014-08-16 08:34:14 -0700

    Adding sequence list

---
+++ LinearList_SequenceList/LinearList_SequenceList.cpp
@@ -0,0 +1,80 @@
+#include "iostream.h"
+#include "stdlib.h"
+/* 线性表的动态分配顺序存储结构 */
+#define initlistsize 100	// 线性表存储空间的初始分配量
+#define increment 10		// 线性表存储空间的分配增量
+
+using namespace std;
+
+typedef struct
+{
+	int *elem;			// 存储空间基址
+	int listsize;		// 当前分配的存储容量(以sizeof(ElemType)为单位)
+	int length;			// 当前长度
+}sqlist;
+
+/* 初始化顺序表 */
+void initlist(sqlist &l)
+{
+	l.elem = new int[initlistsize];		// 分配存储空间
+	l.listsize = initlistsize;			// 构造空的顺序表
+	l.length = 0;						// 初始长度为零
+}
+
+/* 设置顺序表长度及各节点的值 */
+void initlistn(sqlist &l, int n)
+{
+	initlist(l);					// 初始化顺序表
+	for (int i = 0; i < n; i++)		// 根据节点个数循环输入节点值
+	{
+		cin >> l.elem[i];
+	}
+	l.length = n;					// 设置节点长度
+}
+
+/* 输出顺序表 */
+void print(sqlist l)
+{
+	for (int i = 0; i < l.length; i++)		// 根据节点长度循环输出节点值
+	{
+		cout << l.elem[i] << " ";
+	}
+	cout << endl;
+}
+
+/* 插入节点 */
+int insertlist(sqlist &l, int i, int e)
+{
+	if (i < 1 || i > l.length)
+	{
+		return 0;
+	}
+	if (l.length >= l.listsize)		// 若节点长度大于或等于顺序表大小，则自增其大小
+	{
+		int *elem1 = new int[l.listsize + increment];	// 临时创建新表
+		for (int i = 0; i < l.length; i++)		// 将旧表节点的值依次赋值到新表
+		{
+			elem1[i] = l.elem[i];
+		}
+		delete[]l.elem;				// 删除旧表
+		l.elem = elem1;				// 新表赋值到顺序表
+		l.listsize += increment;	// 更新顺序表大小
+	}
+	int *p = &l.elem[l.length - 1];		// 队尾指针
+	int *q = &l.elem[i - 1];			// 插入指针位置
+	for (; p >= q; p--)					// 循环向后一位复制各节点的值，直至队尾指针位置小于插入指针位置
+	{
+		*(p + 1) = *p;
+	}
+	l.elem[i - 1] = e;		// 对插入指针位置赋值
+	l.length++;				// 节点长度自加
+	return 1;
+}
+
+void main(void)
+{
+	sqlist l;
+	initlistn(l, 10);
+	print(l);
+	system("pause");
+}
